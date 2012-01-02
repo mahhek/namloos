39639b8d6a2bb5b959fd09e1205f8e6d2339d810
mahhek <mahhek.khan@gmail.com>
2012-01-02 18:01:33 +0500

    another change

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,5 @@
 class Country < ActiveRecord::Base
-
+  belongs_to :region
   validates :name, :presence => {:message => ' must be selected!'}
   validates :code, :presence => {:message => ' must be selected!'}
   validates :region_id, :presence => {:message => ' must be selected!'}
--- app/models/region.rb
+++ app/models/region.rb
@@ -2,6 +2,8 @@ class Region < ActiveRecord::Base
 
   validates :name, :presence => {:message => 'must be selected!'}
 
+
   has_many :seller_rates
+  has_many :countries
 
 end
