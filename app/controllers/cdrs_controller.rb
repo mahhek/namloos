5c1f3721d591d0a0a0e41bc6755eef32eba3f363
mahhek <mahhek.khan@gmail.com>
2012-01-30 16:02:37 +0500

    changes

--- app/controllers/cdrs_controller.rb
+++ app/controllers/cdrs_controller.rb
@@ -6,10 +6,11 @@ class CdrsController < ApplicationController
     @filter = params[:date_filter].nil? ? "this_month" :  params[:date_filter]
     dates = get_date_range(@filter)
     
-    @seller_rates = SellerRate.all :conditions => ["valid_from >= ? AND valid_to <= ? AND apply_to_id = ? ",
-      dates[0], dates[1], current_user.business_class
-    ]    
+    @seller_rates = SellerRate.all :conditions => ["apply_to_id = ? ",
+      current_user.business_class
+    ]
     @cdrs = {}
+
     @seller_rates.each do |seller_rate|
       cdr_records = Cdr.all :conditions => ["dst like ? AND accountcode = ? AND calldate BETWEEN  ? AND ? AND ( dstchannel like ? OR dstchannel like ? )",
         "#{seller_rate.prefix}%",
